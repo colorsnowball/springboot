1a70dea478c515f9a3e6e6441230ecaaece7fe8c
colorsnowball <gunho604@gmail.com>
2021-03-12 10:57:35 +0900

    gitignore 등록

--- src/main/java/com/springboot/book/Application.java
+++ src/main/java/com/springboot/book/Application.java
@@ -3,8 +3,6 @@ package com.springboot.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
